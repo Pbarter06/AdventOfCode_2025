0f9fc05222bfd7a215f7987a1c7d844904b301d5
Paula <pbarter@upv.edu.es>
2025-12-08 23:02:01 +0100

    Día 5 hecho , Parte 1 y 2 + vs2 para la parte 1

---
+++ otros/Paula/Day6.Paula/Day6.Paula.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    vector<string> M; 
+    string line; 
+
+    while(getline(cin, line)){
+        M.push_back(line); 
+    }
+
+
+
+    return 0; 
+}
