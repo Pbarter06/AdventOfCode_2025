ce0a4336ccb059274edd116a3bfec22a5d5dbd90
Paula <pbarter@upv.edu.es>
2025-12-09 00:15:44 +0100

    Días 6 y 7 parte 1

--- otros/Paula/Day6.Paula/Day6.Paula.cpp
+++ otros/Paula/Day6.Paula/Day6.Paula.cpp
@@ -1,17 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main(){
-
     vector<string> M; 
     string line; 
-
+    
     while(getline(cin, line)){
         M.push_back(line); 
     }
-
-
-
+    
+    int rows = M.size();
+    int cols = 0;
+    for(const auto& row : M) cols = max(cols, (int)row.size());
+    
+    vector<pair<int,int>> problems;
+    int start = 0;
+    
+    for(int col = 0; col <= cols; col++){
+        bool blank = (col == cols);
+        if(col < cols){
+            blank = true;
+            for(int row = 0; row < rows; row++){
+                if(col < (int)M[row].size() && M[row][col] != ' '){
+                    blank = false;
+                    break;
+                }
+            }
+        }
+        
+        if(blank && col > start){
+            problems.push_back({start, col - 1});
+            start = col + 1;
+        }
+    }
+    
+    long long total = 0;
+    
+    for(auto [sc, ec] : problems){
+        vector<long long> nums;
+        char op = ' ';
+        
+        for(int row = 0; row < rows - 1; row++){
+            string num = "";
+            for(int col = sc; col <= ec; col++){
+                if(col < (int)M[row].size() && isdigit(M[row][col])){
+                    num += M[row][col];
+                } else if(!num.empty()){
+                    nums.push_back(stoll(num));
+                    num = "";
+                }
+            }
+            if(!num.empty()) nums.push_back(stoll(num));
+        }
+        
+        for(int col = sc; col <= ec; col++){
+            if(col < (int)M[rows-1].size() && (M[rows-1][col] == '+' || M[rows-1][col] == '*')){
+                op = M[rows-1][col];
+                break;
+            }
+        }
+        
+        if(!nums.empty() && op != ' '){
+            long long res = nums[0];
+            for(size_t i = 1; i < nums.size(); i++){
+                if(op == '+') res += nums[i];
+                else res *= nums[i];
+            }
+            total += res;
+        }
+    }
+    
+    cout << total << "\n";
     return 0; 
 }
---
+++ otros/Paula/Day7.Paula/Day7.Paula.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> grid;
+vector<vector<bool>> dp;
+int R, C;
+long long splits = 0;
+
+
+//PD
+void explore(int r, int c) {
+    // Caso base
+    if (r >= R || c < 0 || c >= C || dp[r][c]) return;
+
+
+    dp[r][c] = true;
+    
+    if (grid[r][c] == '^') {
+        splits++;
+        explore(r + 1, c - 1);
+        explore(r + 1, c + 1);
+    } else {
+        explore(r + 1, c);
+    }
+}
+
+int main() {
+    string line;
+    int startR = 0, startC = 0;
+
+    while (getline(cin, line)) {
+        int pos = line.find('S'); 
+        
+        if (pos != string::npos) {
+            startR = grid.size();
+            startC = pos;
+        }
+        grid.push_back(line);
+    }
+
+    R = grid.size();
+    C = grid[0].size();
+    
+    dp.assign(R, vector<bool>(C, false));
+
+    explore(startR, startC);
+
+    cout << splits << endl;
+    return 0;
+}
